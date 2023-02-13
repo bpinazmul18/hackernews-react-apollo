5a017e2e33523b010475fd0515406b2d1f2d452b
Nazmul Haque <bpinazmul18@gmail.com>
2023-02-13 21:14:22 +0600

    SETUP: Apollo Client.

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../styles/app.css'
+
+const App = () => {
+  return <div>Hello world</div>
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client'
+
+import App from './components/App'
+
+import './styles/index.css'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<h1>Hello world</h1>)
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
